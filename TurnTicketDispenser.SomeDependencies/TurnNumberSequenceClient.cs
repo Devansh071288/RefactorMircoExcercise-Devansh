a1aeed032986b380989dda59265f9c48f0f95bcc
Devansh071288 <devansh.bali@gmail.com>
2023-06-19 19:55:27 +0530

    First commit

---
+++ TurnTicketDispenser.SomeDependencies/TurnNumberSequenceClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TDDMicroExercises.TurnTicketDispenser.SomeDependencies
+{
+    public class TurnNumberSequenceClient
+    {
+        // A class with the only goal of simulating a dependency on TurnNumberSequence
+        // that has impact on the refactoring.
+
+        public TurnNumberSequenceClient()
+        {
+            FindNextUniqueNumber();
+        }
+
+        private void FindNextUniqueNumber()
+        {
+            int nextUniqueTicketNumber;
+            nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
+            nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
+            nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
+        }
+    }
+
+}
---
+++ UnicodeFileToHtmlTextConverter.SomeDependencies/aTextConverterClient1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter.SomeDependencies
+{
+    public class aTextConverterClient1
+    {
+        // A class with the only goal of simulating a dependency on UnicodeFileToHtmTextConverter
+        // that has impact on the refactoring.
+
+        private readonly UnicodeFileToHtmlTextConverter _textConverter;
+        public aTextConverterClient1(UnicodeFileToHtmlTextConverter textConverter)
+        // An IOC container will be responsible for providing this dependency via constructor injection
+        {
+            _textConverter = textConverter;
+            ConvertSampleFileToHtml();
+        }
+
+        private void ConvertSampleFileToHtml()
+        {
+            var filename = "aFilename.txt";
+
+            var html = _textConverter.ConvertToHtml(filename);
+        }
+    }
+}
